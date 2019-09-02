c2b60494d2f097f507a964842d7029b6a048a8bf
Repo <ultrapoolcom@gmail.com>
2019-09-01 20:21:39 -0500

    Source version change

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -56,7 +56,7 @@ const std::string CLIENT_NAME("LeisureCoin Core");
     "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-g" commit
 
 #define BUILD_DESC_FROM_UNKNOWN(maj, min, rev, build) \
-    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-unk"
+    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-LSR"
 
 #ifndef BUILD_DESC
 #ifdef BUILD_SUFFIX
